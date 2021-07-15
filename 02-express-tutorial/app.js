60466cd8cb86e017dc713d507b5ff8667dad0a95
Pranav0210 <pranavpatel.pranshu@gmail.com>
2021-07-15 22:20:16 +0530

    retest changes

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,9 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+console.log(require('http').STATUS_CODES)
+const server = http.createServer((req, res)=>{
+    console.log(`user hit the server.`);
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.end(`Hey! This is the Home Page`)
+})
+server.listen(5000)
